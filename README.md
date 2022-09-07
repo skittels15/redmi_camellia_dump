ab87ebaf632e8400f750487ab7e28f575ca01e28
DroidDumps <DroidDumps@github.com>
2022-09-07 20:46:12 +0300

    Add extras for camellia-user 12 SP1A.210812.016 V13.0.2.0.SKSMIXM release-keys
    Signed-off-by: DroidDumps <DroidDumps@github.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## camellia-user 12 SP1A.210812.016 V13.0.2.0.SKSMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6833
+- Codename: camellia
+- Brand: Redmi
+- Flavor: camellia-user
+- Release Version: 12
+- Id: SP1A.210812.016
+- Incremental: V13.0.2.0.SKSMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/camellia/camellia:12/SP1A.210812.016/V13.0.2.0.SKSMIXM:user/release-keys
+- OTA version: 
+- Branch: camellia-user-12-SP1A.210812.016-V13.0.2.0.SKSMIXM-release-keys
+- Repo: redmi_camellia_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cat .git/objects/pack/pack-a4d89877e8fd65b810f21a18b5d16e5ac7526a4c.pack.* 2>/dev/null >> .git/objects/pack/pack-a4d89877e8fd65b810f21a18b5d16e5ac7526a4c.pack
+rm -f .git/objects/pack/pack-a4d89877e8fd65b810f21a18b5d16e5ac7526a4c.pack.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/data-app/Photos/Photos.apk.* 2>/dev/null >> product/data-app/Photos/Photos.apk
+rm -f product/data-app/Photos/Photos.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null >> system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk
+rm -f system/system/app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk
+rm -f system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
